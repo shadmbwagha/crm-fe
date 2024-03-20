d8804fe7b10ba39650fe0f420edbc8f9b746cb95
shad mbwagha <shadracksylvestar@gmail.com>
2024-03-20 17:45:15 +0300

    main

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import './index.css'
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
 
-
-
 createApp(App).use(router).use(VueToast, {
     position: 'bottom-right',
   }).mount('#app')
